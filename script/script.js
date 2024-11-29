1199af402cc41fe94fb27e009d0196b7b148e57b
Ashvin Antyah <a.antyah@gmail.com>
2024-11-29 13:43:23 +0100

    update js
    -fonction fetch
    -affichage de "advice" et "id"
    -ajout de l'evenement sur le bouton

--- script/script.js
+++ script/script.js
@@ -1,3 +1,20 @@
 const adviceId = document.getElementById("advice_id");
 const advice = document.getElementById("advice");
 const generatorBtn = document.getElementById("generatorBtn");
+
+async function fetchAdvice() {
+    try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        advice.innerHTML = `"${data.slip.advice}"`
+        adviceId.innerHTML=`ADVICE #${data.slip.id} `
+      } catch (error) {
+        console.error('Erreur :', error);
+      }
+}
+fetchAdvice()
+
+generatorBtn.addEventListener('click',()=>{
+    fetchAdvice()
+} 
+)
